4c71d8085591de71de4920ff673d2f3b31ef84df
Phuoc Nguyen <phuoc94@hotmail.fi>
2020-01-31 20:04:57 +0200

    Readme

--- src/components/querys/logo.js
+++ src/components/querys/logo.js
@@ -18,7 +18,7 @@ const Logo = () => {
     query {
       placeholderImage: file(relativePath: { eq: "logo.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 200) {
             ...GatsbyImageSharpFluid
           }
         }
