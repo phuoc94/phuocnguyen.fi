bae26cf51517dbf02d71b859a9f0b9ad3e072775
phuoc <phuoc94@hotmail.fi>
2020-02-05 13:48:03 +0200

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,35 @@ const title = "Contact me"
 const ContactPage = () => (
   <Layout pageTitle={title}>
     <SEO title={title} />
-    <h1>{title}</h1>
+    <div className="contact">
+      <h1>{title}</h1>
+      <div className="form">
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <div className="form-input">
+            <label>Full Name</label>
+            <input name="name" placeholder="Enter Your Name" type="text" />
+          </div>
+          <div className="form-input">
+            <label>Email</label>
+            <input name="email" placeholder="Enter Your Emai" type="email" />
+          </div>
+          <div className="form-input">
+            <label>message</label>
+            <textarea
+              name="message"
+              placeholder="Enter Your Message"
+            ></textarea>
+          </div>
+
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   </Layout>
 )
 
