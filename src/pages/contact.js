7fed905aa6e76aee2629ec492cf9f2a4e7948cfc
phuoc <phuoc94@hotmail.fi>
2020-02-07 12:17:53 +0200

    asd

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,10 +15,6 @@ const ContactPage = () => (
           data-netlify-recaptcha="true"
           data-netlify="true"
         >
-          <div
-            class="g-recaptcha"
-            data-sitekey="6LdZqdYUAAAAALE7UwXWHToWldRPOOuWjdxh6n0E"
-          ></div>
           <input type="hidden" name="form-name" value="JSX Form" />
           <label>
             Name:
@@ -36,7 +32,11 @@ const ContactPage = () => (
             Message:
             <textarea name="message" id="message" rows="5" required />
           </label>
-          <div data-netlify-recaptcha="true"></div>
+          <div
+            data-netlify-recaptcha="true"
+            class="g-recaptcha-response"
+            data-sitekey="6LdZqdYUAAAAALE7UwXWHToWldRPOOuWjdxh6n0E"
+          ></div>
           <button type="submit">Send</button>
         </form>
       </div>
