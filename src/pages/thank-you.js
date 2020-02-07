9f74db687e14beb9f198bfc94df2252f6fbc9803
phuoc <phuoc94@hotmail.fi>
2020-02-07 11:59:02 +0200

    thanks you fix

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,8 +1,9 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/layouts/layout"
 
+const title = "Thank you"
 const ThankYouPage = () => (
-  <Layout>
+  <Layout pageTitle={title}>
     <h1>Contact</h1>
     <p>Thank you for your submission!</p>
   </Layout>
