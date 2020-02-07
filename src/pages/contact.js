86e5c0d1b6406ee39b76ee89c7e6213cbc008aa9
phuoc <phuoc94@hotmail.fi>
2020-02-07 12:24:50 +0200

    asd

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,6 @@ import React from "react"
 import Layout from "../components/layouts/layout"
 import SEO from "../components/querys/seo"
 
-;<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
 const title = "Contact me"
 const ContactPage = () => (
   <Layout pageTitle={title}>
@@ -34,12 +32,11 @@ const ContactPage = () => (
             Message:
             <textarea name="message" id="message" rows="5" required />
           </label>
-          <div data-netlify-recaptcha="true">
-            <div
-              class="g-recaptcha"
-              data-sitekey="6LdZqdYUAAAAALE7UwXWHToWldRPOOuWjdxh6n0E"
-            ></div>
-          </div>
+          <div
+            data-netlify-recaptcha="true"
+            class="g-recaptcha"
+            data-sitekey="6LdZqdYUAAAAALE7UwXWHToWldRPOOuWjdxh6n0E"
+          ></div>
           <button type="submit">Send</button>
         </form>
       </div>
