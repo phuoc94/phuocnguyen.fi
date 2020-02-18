84eec1f0ceac9c04c53a9705872567f23dfa943f
phuoc <phuoc94@hotmail.fi>
2020-02-18 13:26:28 +0200

    tect

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,14 @@ const ContactPage = () => (
     <div className="contact">
       <h1>{title}</h1>
       <div className="form">
-        <form name="contact" method="post" action="/thank-you">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-recaptcha="true"
+          action="/thank-you"
+        >
+          <input type="hidden" name="form-name" value="JSX Form" />
           <label>
             Name:
             <input type="text" name="name" id="name" required />
@@ -26,6 +33,7 @@ const ContactPage = () => (
             Message:
             <textarea name="message" id="message" rows="5" required />
           </label>
+          <div data-netlify-recaptcha="true"></div>
           <button type="submit">Send</button>
         </form>
       </div>
