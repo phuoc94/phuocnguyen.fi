b47bb352ef0f1998e3878d3f120157552055641f
phuoc <phuoc94@hotmail.fi>
2020-02-20 09:13:13 +0200

    debug

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,12 +9,7 @@ const ContactPage = () => (
     <div className="contact">
       <h1>{title}</h1>
       <div className="form">
-        <form
-          name="Contact Form"
-          method="POST"
-          data-netlify="true"
-          data-netlify-recaptcha="true"
-        >
+        <form name="Contact Form" method="POST" data-netlify="true">
           <input type="hidden" name="form-name" value="Contact Form" />
           <label>
             Name:
@@ -32,7 +27,6 @@ const ContactPage = () => (
             Message:
             <textarea name="message" id="message" rows="5" required />
           </label>
-          <div data-netlify-recaptcha="true"></div>
           <button type="submit"> Send</button>
         </form>
       </div>
